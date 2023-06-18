0f5533b9084ba559ce691945b568414891d39f1b
Gabriel Leonardo Martins <101340379+Ninjaalpha01@users.noreply.github.com>
2023-06-17 22:15:48 -0300

    Co-authored-by: Guilherme Quintero Lorenzi <guiquintero@users.noreply.github.com>

--- ProjetoProgApp/src/dao/CategoriaDao.java
+++ ProjetoProgApp/src/dao/CategoriaDao.java
@@ -2,10 +2,12 @@ package dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import db.BancoDados;
 import entities.Categoria;
+import entities.Rendimento;
 
 public class CategoriaDao {
     private Connection conn;
@@ -20,6 +22,61 @@ public class CategoriaDao {
         try {
             statement = conn.prepareStatement("insert into categoria (nome) values (?)");
             statement.setString(1, categoria.getNome());
+            statement.executeUpdate();
+        } finally {
+            BancoDados.finalizarStatement(statement);
+            BancoDados.desconectar();
+        }
+    }
+
+    public Categoria buscarPorId(int idCategoria) throws SQLException {
+        PreparedStatement statement = null;
+        ResultSet result = null;
+
+        try {
+            statement = conn.prepareStatement("select * from categoria where id = ?");
+            statement.setInt(1, idCategoria);
+            result = statement.executeQuery();
+
+            if (result.next()) {
+                Categoria categoria = new Categoria();
+
+                categoria.setId(result.getInt("id"));
+                categoria.setNome(result.getString("nome"));
+
+                return categoria;
+            }
+        } finally {
+            BancoDados.finalizarStatement(statement);
+            BancoDados.finalizarResultSet(result);
+            BancoDados.desconectar();
+        }
+        return null;
+    }
+
+    public void update(Categoria categoria) throws SQLException {
+        PreparedStatement statement = null;
+
+        try {
+            statement = conn.prepareStatement(
+                    "update rendimento set nome = ? where id = ?");
+
+            statement.setString(1, categoria.getNome());
+
+            statement.executeUpdate();
+        } finally {
+            BancoDados.finalizarStatement(statement);
+            BancoDados.desconectar();
+        }
+    }
+
+    public void delete(int idCategoria) throws SQLException {
+        PreparedStatement statement = null;
+
+        try {
+            statement = conn.prepareStatement("delete from categoria where id = ?");
+            statement.setInt(1, idCategoria);
+
             statement.executeUpdate();
         } finally {
             BancoDados.finalizarStatement(statement);
---
+++ ProjetoProgApp/src/service/RendimentoService.java
@@ -0,0 +1,36 @@
+package service;
+
+import entities.Rendimento;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import db.BancoDados;
+import dao.RendimentoDao;
+import java.util.List;
+
+public class RendimentoService {
+    public RendimentoService() {
+    }
+
+    public void createRedimento(Rendimento rendimento) throws SQLException, IOException {
+        Connection conn = BancoDados.conectar();
+        new RendimentoDao(conn).create(rendimento);
+    }
+
+    public void updateRedimento(Rendimento rendimento) throws SQLException, IOException {
+        Connection conn = BancoDados.conectar();
+        new RendimentoDao(conn).update(rendimento);
+    }
+
+    public void deleteRedimento(int id) throws SQLException, IOException {
+        Connection conn = BancoDados.conectar();
+        new RendimentoDao(conn).delete(id);
+    }
+
+    public List<Rendimento> findAllRedimentos() throws SQLException, IOException {
+        Connection conn = BancoDados.conectar();
+        return new RendimentoDao(conn).findAll();
+    }
+}
