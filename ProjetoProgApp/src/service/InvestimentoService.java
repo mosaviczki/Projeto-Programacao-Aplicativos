960f3609655d7f68eaef96e0b38fe588a6a5f139
Gabriel Leonardo Martins <101340379+Ninjaalpha01@users.noreply.github.com>
2023-06-17 23:12:54 -0300

    up

--- ProjetoProgApp/src/service/InvestimentoService.java
+++ ProjetoProgApp/src/service/InvestimentoService.java
@@ -4,7 +4,6 @@ import entities.Investimento;
 
 import java.io.IOException;
 import java.sql.Connection;
-import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import db.BancoDados;
 import dao.InvestimentoDao;
