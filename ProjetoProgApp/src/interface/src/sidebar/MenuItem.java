92a8e20efe4892265f4f0980b82cae12fe5bdaa6
Gabriel Leonardo Martins <101340379+Ninjaalpha01@users.noreply.github.com>
2023-06-21 21:04:40 -0300

    up fixing

--- ProjetoProgApp/src/interface/src/sidebar/MenuItem.java
+++ ProjetoProgApp/src/interface/src/sidebar/MenuItem.java
@@ -48,8 +48,6 @@ public class MenuItem extends javax.swing.JPanel {
         }
     }//GEN-LAST:event_formMousePressed
     
-    @SuppressWarnings("unchecked")
-    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
         jSeparator1 = new javax.swing.JSeparator();
